d72ac134d7fed8484dbcee0ccc9c864118fefd05
Stephen Prokopenko <prkstep@gmail.com>
2022-05-21 23:23:03 +0300

    Update
    Impoved animations

---
+++ app/js/anim.js
@@ -0,0 +1,64 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const pageContainer = document.querySelector(".globalWrapper");
+
+/* SMOOTH SCROLL */
+const scroller = new LocomotiveScroll({
+  el: pageContainer,
+  smooth: true,
+  lerp: 0.05,
+  multiplier: 1.5
+});
+
+scroller.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(pageContainer, {
+  scrollTop(value) {
+    return arguments.length
+      ? scroller.scrollTo(value, 0, 0)
+      : scroller.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {
+      left: 0,
+      top: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  },
+  pinType: pageContainer.style.transform ? "transform" : "fixed"
+});
+
+////////////////////////////////////
+////////////////////////////////////
+window.addEventListener("load", function () {
+  let pinBoxes = document.querySelectorAll(".pin-wrap > *");
+  let pinWrap = document.querySelector(".pin-wrap");
+  let pinWrapWidth = pinWrap.offsetWidth;
+  let horizontalScrollLength = pinWrapWidth - window.innerWidth;
+
+  // Pinning and horizontal scrolling
+
+  gsap.to(".pin-wrap", {
+    scrollTrigger: {
+      scroller: pageContainer, //locomotive-scroll
+      scrub: true,
+      trigger: "#sectionPin",
+      pin: true,
+      // anticipatePin: 1,
+      start: "top top",
+      markers: true,
+      end: () => document.getElementsByClassName('hero').offsetHeight + document.getElementsByClassName('projects').offsetHeight + document.getElementsByClassName('fullscreen').offsetHeight + document.querySelector(".pin-wrap").offsetHeight,
+    },
+    x: -horizontalScrollLength,
+    ease: "none"
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
+
+  ScrollTrigger.refresh();
+});
+
+let scrollFix = document.getElementsByClassName(".globalWrapper");
+
+scrollFix.style.overflow = hidden;
